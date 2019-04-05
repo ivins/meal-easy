bfc6634f32c583555ab05a6fa21760600fb4cde7
Clive Ivins <seelive.i@gmail.com>
2019-04-05 12:33:54 -0700

    initialized dragdrop context

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 import './styles/App.css';
 import MealsContainer from './MealsContainer'
 import ViewContainer from './ViewContainer'
+import initialData from './initialData'
+import { DragDropContext } from 'react-beautiful-dnd';
 
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      initialData
+    }
+  }
+
+  onDragEnd = result => {
+    // Coming back here to handle state changes
   }
 
   render() {
@@ -14,10 +22,13 @@ class App extends Component {
       <div className="App">
         <nav>
           <h1>Meal Easy</h1>
+          <p>{this.state.initialData.columns['column-1'].mealIds}</p>
         </nav>
         <section>
-        <MealsContainer />
-        <ViewContainer />
+          <DragDropContext onDragEnd={this.onDragEnd}>
+            <MealsContainer />
+            <ViewContainer />
+          </DragDropContext>
         </section>
       </div>
     );
--- src/MealsContainer.jsx
+++ src/MealsContainer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
 import classnames from 'classnames';
+import { Droppable } from 'react-beautiful-dnd';
 
 export default class Example extends React.Component {
   constructor(props) {
@@ -8,7 +9,7 @@ export default class Example extends React.Component {
 
     this.toggle = this.toggle.bind(this);
     this.state = {
-      activeTab: '2'
+      activeTab: '1'
     };
   }
 
@@ -28,27 +29,28 @@ export default class Example extends React.Component {
               className={classnames({ active: this.state.activeTab === '1' })}
               onClick={() => { this.toggle('1'); }}
             >
-              Last Week
+              This Weeks Meal Plan
             </NavLink>
           </NavItem>
-          <NavItem>
+          {/* <NavItem>
             <NavLink
               className={classnames({ active: this.state.activeTab === '2' })}
               onClick={() => { this.toggle('2'); }}
             >
               This Week
             </NavLink>
-          </NavItem>
+          </NavItem> */}
         </Nav>
         <TabContent activeTab={this.state.activeTab}>
-          <TabPane tabId="1">
+          {/* <TabPane tabId="1">
             <Row>
               <Col sm="12">
                 <h4>Tab 1 Contents</h4>
               </Col>
             </Row>
-          </TabPane>
-          <TabPane tabId="2">
+          </TabPane> */}
+          <TabPane tabId="1">
+          {/* <Droppable> */}
             <Row>
               <Col sm="11" className="mealCard">
                 <Card body>
@@ -60,12 +62,21 @@ export default class Example extends React.Component {
               <Col sm="11" className="mealCard">
                 <Card body>
                   <CardTitle>Special Title Treatment</CardTitle>
-                  <img src="https://spoonacular.com/recipeImages/537176-240x150.jpg" alt="food"/>
+                  {/* <img src="https://spoonacular.com/recipeImages/537176-240x150.jpg" alt="food"/> */}
+                  <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                  <Button>Go somewhere</Button>
+                </Card>
+              </Col>
+              <Col sm="11" className="mealCard">
+                <Card body>
+                  <CardTitle>Special Title Treatment</CardTitle>
+                  {/* <img src="https://spoonacular.com/recipeImages/537176-240x150.jpg" alt="food"/> */}
                   <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
                   <Button>Go somewhere</Button>
                 </Card>
               </Col>
             </Row>
+            {/* </Droppable> */}
           </TabPane>
         </TabContent>
       </div>
